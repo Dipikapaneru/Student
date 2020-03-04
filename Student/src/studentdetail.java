8ef0791c80339738216b202e0ca9002b437ec95e
Dipika <Dipika@keshabdipika.lan>
2020-03-04 15:56:32 -0500

    this is first comment

---
+++ Student/src/studentdetail.java
@@ -0,0 +1,15 @@
+
+public class studentdetail {
+
+	public static void main(String[] args) {
+		String name="Dipika";
+		String lastname="Paneru";
+		int age=28;
+		String color="fair";
+		System.out.println(name);
+		System.out.println(lastname);
+		System.out.println(age);
+		System.out.println(color);
+	}
+
+}
